bf7ef35f22b45fcf70c777ae1761f1c78c13a37f
Aakarshak Sthapak <akku_sthapak@live.com>
2024-09-06 13:18:51 +0530

    feat : add toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import DashBoard from '@/components/DashBoard';
+import { Toaster } from '@/components/ui/sonner';
+
+
 
 function App() {
-  return <DashBoard />;
+  return (
+    <>
+      <DashBoard />
+      <Toaster />
+    </>
+  );
 }
 
 export default App;
